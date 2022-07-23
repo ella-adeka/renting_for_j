ea9a5a1c2978f1fd9353379b4c017fe594923b11
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-07-23 23:36:04 +0100

    theme-related components

---
+++ frontend/src/components/Toggler.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { func, string } from 'prop-types';
+// import styled from "styled-components"
+
+const Toggle = ({theme, toggleTheme }) => {
+    return (
+        <Button onClick={toggleTheme} >
+          Switch Theme
+        </Button>
+    );
+};
+Toggle.propTypes = {
+    theme: string.isRequired,
+    toggleTheme: func.isRequired,
+}
+export default Toggle;
