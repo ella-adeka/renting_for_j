23c5c7268ec27a3e1db2384a020a51afc3798756
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-08-01 00:03:44 +0100

    context files

---
+++ frontend/src/utils/context/authContext.js
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+const AuthContext = createContext();
+
+export default AuthContext;
+
+// const AuthProvider = () => {
+//     return(
+//         <p>Is logged in</p>
+//     )
+// }
---
+++ frontend/src/utils/context/wishlistContext.js
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+const WishlistContext = createContext();
+
+export default WishlistContext;
+
+// const AuthProvider = () => {
+//     return(
+//         <p>Is logged in</p>
+//     )
+// }
