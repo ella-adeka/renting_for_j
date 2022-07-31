94be8da907a809ae3c3efb1c2baa37da91422c10
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-08-01 00:22:38 +0100

    updated file path

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import HomeNavigation from "../components/HomeNavigation";
+import HomeNavigation from "../components/Navigation/HomeNavigation";
 
 export default class Home extends Component{
     constructor(props){
