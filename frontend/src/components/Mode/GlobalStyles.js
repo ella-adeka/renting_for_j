3f976364f4dcec58bfe735116e8896789562d2c5
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-08-01 00:04:08 +0100

    controls the themes and globalStyles

---
+++ frontend/src/components/Mode/GlobalStyles.js
@@ -0,0 +1,77 @@
+import { createGlobalStyle} from "styled-components"
+export const GlobalStyles = createGlobalStyle`
+  body {
+    background: ${({ theme }) => theme.body};
+    color: ${({ theme }) => theme.text};
+    transition: all 0.50s linear;
+  }
+  input, a, button, textarea{
+    color:${({theme}) => theme.link};
+    text-decoration: none;
+
+    
+  }
+  .menu_bar{
+    background: ${({ theme }) => theme.body};
+  }
+
+  .menu_bar__div{
+    border: 0.7px solid ${({ theme }) => theme.mainNavLink};
+    background: ${({ theme }) => theme.body};
+    
+  }
+  
+  .menu_bar__div__icon{
+    color: ${({ theme }) => theme.link};
+    opacity: 0.8;
+  }
+  .menu_bar__div__icon:hover{
+    color: ${({theme}) => theme.hoverLink};
+    opacity: 1;
+  }
+  .menu_bar__div:hover{
+    background: ${({ theme }) => theme.mainNavLink};
+  }
+  .mainNavigation{
+    background: ${({ theme }) => theme.body};
+    color:${({theme}) => theme.link};
+  }
+
+  .mainNavigation a{
+    color: ${({theme}) => theme.mainNavLink};
+  }
+  .mainNavigation a:hover{
+    color: ${({theme}) => theme.hoverLink};
+    transition: all 0.2s linear;
+  }
+
+  .react-datepicker {
+    background: ${({ theme }) => theme.body} !important;
+    border-color: 0.7px solid ${({ theme }) => theme.mainNavLink} !important;
+  }
+
+  .react-datepicker__header {
+    background: ${({ theme }) => theme.body} !important;
+  }
+
+  .react-datepicker__day-name{
+    color: ${({ theme }) => theme.text};
+  }
+
+  .react-datepicker__day,
+  .react-datepicker__day-name,
+  .react-datepicker__current-month{
+    color: ${({ theme }) => theme.text};
+  }
+
+  .react-datepicker__day--disabled{
+    background: ${({ theme }) => theme.body} !important;
+  }
+
+  .highlights__highlight{
+    border: 0.1px solid ${({ theme }) => theme.highlightBorder};
+  }
+  .share_like a:nth-child(1){
+    border-right: 0.7px solid ${({ theme }) => theme.mainNavLink};
+  }
+  `
---
+++ frontend/src/components/Mode/Theme.js
@@ -0,0 +1,26 @@
+
+export const lightTheme = {
+    body: '#dbd9d9',
+    text: '#000000',
+    background: '#dbd9d9',
+    input: '#000000',
+    link: '#000000',
+    mainNavLink: '#7c7c7c',
+    hoverLink: 'rgb(50, 50, 50)',
+    highlightBorder: 'rgba(0, 0, 0, 0.117)'
+
+}
+export const darkTheme = {
+    body: '#121212',
+    text: '#f2eeee',
+    background: '#121212',
+    input: '#f2eeee',
+    link: '#f2eeee',
+    // link: '#646464',
+    mainNavLink: '#646464',
+    hoverLink: '#f1f1f1',
+    highlightBorder: 'rgba(255, 255, 255, 0.117)'
+}
+
+// background-color:  #121212;
+    // color: #f2eeee;
