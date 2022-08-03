8db3a446d9d6017d4d51a13d20b03658c5b3bcbf
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-08-03 20:13:50 +0100

    created a Wishlist to add items to Wishlist and display items in the wishlist

--- frontend/src/utils/context/wishlistContext.js
+++ frontend/src/utils/context/wishlistContext.js
@@ -1,6 +1,19 @@
-import { createContext } from "react";
+import  React,{ createContext, useState } from "react";
 
 const WishlistContext = createContext();
+
+export function WishlistProvider({children}){
+    const [ items, setItems ] = useState([]);
+
+    const addToWishlist = (id, bath, bed, bedroom, city, image,  max_guests, title, type, price, slug) => {
+        setItems((prevState) => [...prevState, { id, bath, bed, bedroom, city, image,  max_guests, title, type, price, slug }]);
+    }
+    return(
+        <WishlistContext.Provider value={{ items, addToWishlist }}>
+            {children}
+        </WishlistContext.Provider>
+    )
+}
 
 export default WishlistContext;
 
