8edde1eba2fbef18d860e865374ada7e3875ef76
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-08-07 22:22:40 +0100

    deactivate user onSubmit

--- frontend/src/pages/Users/DeactivateUserAccount.js
+++ frontend/src/pages/Users/DeactivateUserAccount.js
@@ -5,12 +5,16 @@ export default class DeactivateUserAccount extends Component{
     constructor(props){
         super(props);
         this.state = {
+            user: '',
             email: '',
             first_name: '',
             last_name: '',
             avatar: '',
+            is_active: true,
             loading: true,
         }
+
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
     componentDidMount(){
@@ -28,6 +32,7 @@ export default class DeactivateUserAccount extends Component{
             .then(res => res.json())
             .then(data => {
                 this.setState({
+                    user: data.id,
                     email: data.email,
                     first_name: data.first_name,
                     last_name: data.last_name,
@@ -38,27 +43,50 @@ export default class DeactivateUserAccount extends Component{
         }
     }
 
+    handleSubmit = (event) => {
+        event.preventDefault();
+
+        fetch('http://127.0.0.1:8000/api/v1/users/dj-rest-auth/user/', {
+            // fetch('http://127.0.0.1:8000/api/users/', {
+                method: 'PATCH',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Token ${localStorage.getItem('token')}`
+                },
+                body: JSON.stringify({ is_active : false})
+            })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                localStorage.removeItem("token");
+                window.location.replace('http://127.0.0.1:8000/');
+            })
+            .catch((err) => console.log(err))
+    }
+
     
     render(){
         const { first_name, loading } = this.state;
 
         return(
-            <div>
-               { loading === false && (
-                    <Fragment>
-                        {''}
-                        
-
-                        {/* <h6>Sad to see you go,{first_name} :(</h6> */}
-                        <h6>Sad to see you go :(</h6>
-                        <h3>{first_name}, tell us your reasons</h3>
-                        <p>Don't really like it here :(</p>
-                        <br></br>
-                        <Link className="back_to_props" to={{ pathname: `/user/account`}}>Nevermind</Link>
-                        <button type="submit">Deactivate</button>
-                    </Fragment>
-               )}
-            </div>
+            <main>
+                <div className="signup_page">
+                    { loading === false && (
+                        <Fragment>
+                            {''}
+                            <form onSubmit={this.handleSubmit} className="signup_form">
+                                <h6>Sad to see you go :(</h6>
+                                <h3>{first_name}, tell us your reasons</h3>
+                                <p>Don't really like it here :(</p>
+                                <br></br>
+                                <br></br>
+                                <button className="signup_form__button" type="submit">Deactivate</button>
+                                <button  type="link" className="signup_form__button without"><Link to={{ pathname: '/user/account'}}>Nevermind</Link></button>
+                            </form>
+                        </Fragment>
+                    )}
+                </div>
+            </main>
         )
     }
 }
