aaab3e36f53288d18cc86493d131f41d7201ce0d
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-08-07 22:21:32 +0100

    added removeFromWishlist function

--- frontend/src/utils/context/wishlistContext.js
+++ frontend/src/utils/context/wishlistContext.js
@@ -8,17 +8,16 @@ export function WishlistProvider({children}){
     const addToWishlist = (id, bath, bed, bedroom, city, image,  max_guests, title, type, price, slug) => {
         setItems((prevState) => [...prevState, { id, bath, bed, bedroom, city, image,  max_guests, title, type, price, slug }]);
     }
+
+    const removeFromWishlist = (id) => {
+        setItems((items) => items.filter(i => i == id));
+    }
+
     return(
-        <WishlistContext.Provider value={{ items, addToWishlist }}>
+        <WishlistContext.Provider value={{ items, addToWishlist, removeFromWishlist }}>
             {children}
         </WishlistContext.Provider>
     )
 }
 
 export default WishlistContext;
-
-// const AuthProvider = () => {
-//     return(
-//         <p>Is logged in</p>
-//     )
-// }
