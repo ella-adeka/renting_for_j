188a4520c2df2e419385ead6657c57f4e60a230a
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-08-16 20:05:13 +0100

    preloader for the app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import  ReactDOM from "react-dom/client";
 import reportWebVitals from './reportWebVitals';
 import MyRoutes from "./Routes";
@@ -10,22 +10,42 @@ import {ThemeProvider} from "styled-components";
 import { GlobalStyles } from "./components/Mode/GlobalStyles";
 import { lightTheme, darkTheme } from "./components/Mode/Theme";
 import { useDarkMode } from "./components/Mode/UseDarkMode";
+import Preloader from "./components/Preloader";
 
 const App = () => {
+    const [ loading, setLoading ] = useState(true);
     const [theme, themeToggler, mountedComponent] = useDarkMode();
     const themeMode = theme === 'light' ? lightTheme : darkTheme;
 
+    useEffect(() => {
+        let preLoadTimer = setTimeout(() => {
+            setLoading((false))
+        }, 3000);
+
+        return () => clearTimeout(preLoadTimer)
+    })
+
     if (!mountedComponent) return <div /> 
-    return (
-        // <AuthProvider>
-            <WishlistProvider>
-                <ThemeProvider theme={themeMode}>
-                    <GlobalStyles />
-                    <MyRoutes themeToggler={themeToggler} ></MyRoutes>
-                </ThemeProvider>
-            </WishlistProvider>
-        // </AuthProvider>
-    );  
+    
+    if (loading) {
+        return (
+            <ThemeProvider theme={themeMode}>
+                <GlobalStyles />
+                <Preloader></Preloader>
+            </ThemeProvider>
+        )
+    } else{
+        return (
+            // <AuthProvider>
+                <WishlistProvider>
+                    <ThemeProvider theme={themeMode}>
+                        <GlobalStyles />
+                        <MyRoutes themeToggler={themeToggler} ></MyRoutes>
+                    </ThemeProvider>
+                </WishlistProvider>
+            // </AuthProvider>
+        );  
+    }
 }
 
 export default App;
---
+++ frontend/src/components/Preloader.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Preloader = () => {
+    return(
+        <div>
+            <h2 className="preloader_text">Loading...</h2>
+        </div>
+    )
+}
+
+export default Preloader;
