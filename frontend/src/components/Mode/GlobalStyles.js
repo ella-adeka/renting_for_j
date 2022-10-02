8b9e01b4bbb2f86bc277dd8c62482c1b502fb6b1
Adeka Emmanuella Onyowo <onyiadeka@gmail.com>
2022-10-02 23:22:19 +0100

    added more styles

--- frontend/src/components/Mode/GlobalStyles.js
+++ frontend/src/components/Mode/GlobalStyles.js
@@ -21,6 +21,10 @@ export const GlobalStyles = createGlobalStyle`
     border-width: 0 0 1px 0;
   }
 
+  .error_page{
+    background: ${({ theme }) => theme.body};
+  }
+
   .profile_photo{
     border: 0.7px solid ${({ theme }) => theme.mainNavLink} ;
   }
@@ -32,6 +36,18 @@ export const GlobalStyles = createGlobalStyle`
 
   .edit_profile_photo:hover{
     filter: invert(15%)
+  }
+
+  .book_now_btn{
+    color: ${({ theme }) => theme.body};
+    background:  ${({ theme }) => theme.hoverLink} ;
+    // backdrop-filter: invert(50%);
+    // filter: invert(100%);
+    transition: all 0.2s linear;
+  }
+  .book_now_btn:hover{
+    background:  ${({ theme }) => theme.text} ;
+    transition: all 0.2s linear;
   }
 
 
@@ -46,24 +62,32 @@ export const GlobalStyles = createGlobalStyle`
     transition: all 0.2s linear;
   }
 
- 
+ .menu_bar{
+  filter: drop-shadow(0 0 0.75rem ${({ theme }) => theme.body});
+ }
+
+
 
   .menu_bar__div,
+  .socials_bar__span,
   .part_menu_bar__li{
     border: 0.7px solid ${({ theme }) => theme.mainNavLink};
     background: ${({ theme }) => theme.body};
+    
+
   }
   
   .menu_bar__div__icon{
     color: ${({ theme }) => theme.link};
-    opacity: 0.8;
+    opacity: 0.7;
   }
   .part_menu_bar__li__link__icon, 
   .part_menu_bar__li__link sup{
     color: ${({ theme }) => theme.link};
   }
+
   .menu_bar__div__icon:hover{
-    filter: invert(100%);
+    // filter: invert(100%);
     opacity: 1;
   }
   .menu_bar__div:hover{
